02753d90eef29fcf361eec0dca32ee7ae236f38a
Gideon <reachgiddy@gmail.com>
2020-01-22 17:21:20 -0500

    Setup useState/useEffect for App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import Axios from "axios";
 
 function App() {
+  const [] = useState();
+
+  useEffect(function(){
+    Axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY").then(response => {
+      console.log(response);
+    })
+  }, []);
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+
+      <PhotoPage />
     </div>
   );
 }
---
+++ src/components/PhotoContainer/PhotoPage.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from "react";
+import "./PhotoPage.css";
+
+function PhotoPage() {
+const 
+
+    return (
+        <Photo />
+    );
+}
+
+export default PhotoPage;
