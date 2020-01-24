f7237efddcaf3ebda647f8f3486a93cb561835eb
Gideon <reachgiddy@gmail.com>
2020-01-23 21:43:39 -0500

    Updated styling for NASA photo of the day

--- src/components/PhotoContainer/PhotoCard.js
+++ src/components/PhotoContainer/PhotoCard.js
@@ -1,54 +1,84 @@
 import React from "react";
 import "./PhotoCard.css";
 import styled from "styled-components";
+// import img from "../../assets/img/space-suit.jpg";
 
 const CardContainer = styled.div`
   width: 100%;
-  height: 110vh;
+  height: 105vh;
   display: flex;
 `;
 
 const DescriptionContainer = styled.div`
-  width: 27%;
-  height: 110vh;
-  color: #636262;
-  font-size: 0.84rem;
-  padding-top: 20px;
+  width: 40%;
+  height: 100vh;
+  color: black;
+  font-size: 0.83rem;
+  padding-top: 10px;
   padding-right: 50px;
   padding-left: 50px;
   box-sizing: border-box;
 `;
 
-const PhotoImg = styled.img`
-  width: 73%;
-  height: 130vh;
-  box-shadow: 0 4px 25px 10px rgba(156, 134, 197, 0.103),
-    0 6px 40px 10px rgba(156, 134, 197, 0.103);
+const Title = styled.h3`
+  font-size: 1.9rem;
 `;
 
-const DescriptionTitle = styled.h3`
-  font-size: 1.7rem;
+const Description = styled.h4`
+  font-size: 0.8rem;
+  background-color: skyblue;
+  color: white;
+  display: inline;
+  padding: 5px 10px;
+  border-radius: 20px;
+  font-weight: light;
 `;
 
-const PhotoExplanation = styled.p`
-  line-height: 1.3rem;
+const Explanation = styled.p`
+  line-height: 1.62rem;
+  font-size: 0.83rem;
+  margin-top: 20px;
 `;
+
+const Footer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: -7px;
+`;
+
+const SpanItem = styled.h4`
+  background-color: skyblue;
+  color: white;
+  padding: 5px 10px;
+  border-radius: 20px;
+  display: inline;
+`;
+
+const Img = styled.img`
+  width: 60%;
+  height: 99vh;
+  border-radius: 5px;
+`;
+
+// const SpanContain = styled.span``;
 
 function PhotoCard(props) {
   //   console.log(props);
   return (
     <CardContainer>
       <DescriptionContainer>
-        <DescriptionTitle>{props.photoData.title}</DescriptionTitle>
-        <h4>Description</h4>
-        <PhotoExplanation>{props.photoData.explanation}</PhotoExplanation>
-        <div>
-          <span>Copyright: {props.photoData.copyright}</span>
-
-          <span>Date: {props.photoData.date}</span>
-        </div>
+        <Title>{props.photoData.title}</Title>
+        <Description>Description</Description>
+        <Explanation>{props.photoData.explanation}</Explanation>
+        <Footer>
+          <SpanItem>
+            Copyright:
+            {props.photoData.copyright}
+          </SpanItem>
+          <SpanItem>Date: {props.photoData.date}</SpanItem>
+        </Footer>
       </DescriptionContainer>
-      <PhotoImg src={props.photoData.url} />
+      <Img src={props.photoData.url} />
     </CardContainer>
   );
 }
